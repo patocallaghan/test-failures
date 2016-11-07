b6c2244ade62e2885f06b523c0a8a3f075a2972a
Pat O'Callaghan <patocallaghan@gmail.com>
2016-11-07 16:59:28 +0000

    fixup!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const TestFailures = require('./lib/test-failures');
-let results = TestFailures.run('seeds/50-partitions');
-fs.writeFile("failed-tests-50-partitions.txt", JSON.stringify(results, null, 2), function(err) {
+let results = TestFailures.run('seeds');
+fs.writeFile("failed-tests.txt", JSON.stringify(results, null, 2), function(err) {
   if(err) {
     return console.log(err);
   }
