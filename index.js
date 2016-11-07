829285d281c68cd85457bc568b8a423b12bdf66f
Pat O'Callaghan <patocallaghan@gmail.com>
2016-11-07 23:38:02 +0000

    fixup!

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 const TestFailures = require('./lib/test-failures');
 var args = process.argv.slice(2);
-console.log('params: ', args[0]);
+
 let results = TestFailures.run(args[0]);
-fs.writeFile("failed-tests.txt", JSON.stringify(results, null, 2), function(err) {
+fs.writeFile(args[1], JSON.stringify(results, null, 2), function(err) {
   if(err) {
     return console.log(err);
   }
-  console.log("Results written to failed-tests.txt");
+  console.log(`Results written to ${args[1]}`);
 });
